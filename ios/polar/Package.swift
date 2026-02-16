160fd83c2ca480eb3c433d4db251b35a44828bc9
Rexios <rexios@rexios.dev>
2026-02-16 16:58:08 -0500

    Swift format

--- ios/polar/Package.swift
+++ ios/polar/Package.swift
@@ -4,22 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "polar",
-    platforms: [
-      .iOS("14.0")
-    ],
-    products: [
-        .library(name: "polar", targets: ["polar"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/polarofficial/polar-ble-sdk.git", .exact("6.14.0"))
-    ],
-    targets: [
-        .target(
-            name: "polar",
-            dependencies: [
-                .product(name: "PolarBleSdk", package: "polar-ble-sdk")
-            ],
-        )
-    ]
+  name: "polar",
+  platforms: [
+    .iOS("14.0")
+  ],
+  products: [
+    .library(name: "polar", targets: ["polar"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/polarofficial/polar-ble-sdk.git", .exact("6.14.0"))
+  ],
+  targets: [
+    .target(
+      name: "polar",
+      dependencies: [
+        .product(name: "PolarBleSdk", package: "polar-ble-sdk")
+      ],
+    )
+  ]
 )
