d28a11f18b78d2090ef6ea67ba84e95643d3c7db
Rexios <rexios@rexios.dev>
2026-09-08 21:35:02 -0400

    Fix shutdown test

--- test/tests.dart
+++ test/tests.dart
@@ -299,9 +299,7 @@ void testMisc(String identifier, {required bool supportsLedConfig}) {
 void testShutDown(String identifier) {
   test('shutDown', () async {
     await connect(identifier);
-    await polar.shutDown();
-    await connect(identifier);
-    await disconnect(identifier);
+    expect(polar.shutDown(), completes);
   });
 }
 
